1fd3ab9f1387922a5f57fc8f9acce7c5508cc791
cole nelson <colionelson@gmail.com>
2023-05-15 09:07:18 -0600

    Lint Config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr(), tsconfigPaths()],
+    plugins: [react(), svgr(), tsconfigPaths(), eslint()],
     server: {
         proxy: {
             '/api': {
